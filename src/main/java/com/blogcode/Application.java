c11c8f1f86a374942d9a7b225491175963ed5799
hajubal <30306097+hajubal@users.noreply.github.com>
2018-11-13 16:06:58 +0900

    Update Application.java
    주석, 로그 추가.

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+//주석 추가
 	public static void main(String[] args) {
+		System.out.println("Startup.");
 		SpringApplication.run(Application.class, args);
 	}
 }
