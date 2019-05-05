1973ff1f80882fae365893a07ccf46a35a00aa97
hajubal <30306097+hajubal@users.noreply.github.com>
2019-05-05 20:15:32 +0900

    Update Application.java
    added console log. "started"

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -8,6 +8,9 @@ public class Application {
 //주석 추가
 	public static void main(String[] args) {
 		System.out.println("Startup!!!!");
+		
 		SpringApplication.run(Application.class, args);
+		
+		System.out.println("Started!!!!");
 	}
 }
